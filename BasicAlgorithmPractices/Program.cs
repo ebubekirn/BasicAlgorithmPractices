e8bd1d90d9352ec8275a0bde7070704784b23dda
null <null@gmail.com>
2023-01-10 20:49:31 +0300

    Asal sayı bulma komutu eklendi

--- BasicAlgorithmPractices/Program.cs
+++ BasicAlgorithmPractices/Program.cs
@@ -128,6 +128,8 @@ for (int i = 0; i < number; i++) // 1. for
 
 #region Çember Çizdirme
 
+/*
+
 double radius;
 double thickness = 0.2;
 // ConsoleColor BorderColor = ConsoleColor.Yellow;
@@ -171,4 +173,69 @@ for (int i = 1; i < radius * 2 + 2; i++)
 }
 Console.ReadKey();
 
+*/
+
 #endregion
+
+class Program
+{
+    public static bool asalmi(int sayi)
+    {
+        bool durum = false;
+
+        int kontrol = 0;
+
+        for (int i = 2; i < sayi; i++)
+        {
+            if (sayi % i == 0) //www.yazilimkodlama.com
+            {
+                kontrol = 1;
+                break;
+            }
+        }
+        if (kontrol == 1)
+        {
+            durum = false;
+        }
+        else
+        {//www.yazilimkodlama.com
+            durum = true;
+        }
+
+        return durum;
+    }
+    static void Main(string[] args)
+    {
+        int sayac = 0;
+        Console.Write("Sayıyı Girin : ");
+        int s = Convert.ToInt32(Console.ReadLine());
+
+        if (asalmi(s))
+        {
+            Console.WriteLine("{0} sayısı Asal", s);
+        }
+        else
+        {
+            Console.WriteLine("{0} sayısı asal değil", s);
+        }
+
+        Console.WriteLine("===========================");
+        Console.WriteLine("Sonraki 5 Asal Sayı");
+        Console.WriteLine("===========================");
+
+        while (true)
+        {
+            s++;
+            if (asalmi(s) == true)
+            {
+                Console.WriteLine("{0} sayısı asal", s);
+                sayac++;
+                if (sayac == 5)
+                {
+                    break;
+                }
+            }
+        }
+        Console.ReadKey();
+    }
+}
